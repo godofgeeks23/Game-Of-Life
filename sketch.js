c23981bcc803f65010961053bc18492ba9f3bf05
Aviral Srivastava <aviralji4@gmail.com>
2022-04-09 23:19:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,19 @@ function make2DArray(cols, rows) {
 	return arr;
 }
 
+function countNeighbors(grid, i, j) {
+	let sum = 0;
+	for (let x = -1; x < 2; x++) {
+		for (let y = -1; y < 2; y++) {
+			let col = (i + x + cols) % cols;
+			let row = (j + y + rows) % rows;
+			sum += grid[col][row];
+		}
+	}
+	sum -= grid[i][j];
+	return sum;
+}
+
 function setup() {
 	createCanvas(cols * cell_size, rows * cell_size);
 	grid = make2DArray(cols, rows);
@@ -36,4 +49,25 @@ function draw() {
 			rect(i * cell_size, j * cell_size, cell_size, cell_size);
 		}
 	}
+
+	let next = make2DArray(cols, rows);
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			let neighbours = countNeighbors(grid, i, j);
+			let state = grid[i][j];
+
+			if (state == 0 && neighbours == 3) {
+				next[i][j] = 1;
+			}
+			else if (state == 1 && (neighbours < 2 || neighbours > 3)) {
+				next[i][j] = 0;
+			}
+			else {
+				next[i][j] = state;
+			}
+		}
+	}
+
+	grid = next;
+
 }
