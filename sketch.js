5c9b496a2ed86df967ce42a3a43a8b01b496f0c4
Aviral Srivastava <aviralji4@gmail.com>
2022-04-09 22:49:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
-var angle;
+let grid;
+let rows = 30;
+let cols = 30;
+let cell_size = 20;
+
+function make2DArray(cols, rows) {
+	let arr = new Array(cols);
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = new Array(rows);
+	}
+	return arr;
+}
+
 function setup() {
-  createCanvas(640, 480,WEBGL);
-  angle = 0.0;
+	createCanvas(cols * cell_size, rows * cell_size);
+	grid = make2DArray(cols, rows);
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j] = floor(random(2));
+		}
+	}
 }
 
 function draw() {
-	background(51);
-	fill(mouseX,mouseY,mouseX+mouseY);
-	rotateX(-angle);
-	rotateY(-angle);
-  	box(180);
-  	angle = angle+0.009;
+	background(0);
+	stroke(255);
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			if (grid[i][j] == 1) {
+				fill(255);
+				// stroke(255);
+			} else {
+				fill(0);
+				// stroke(0);
+			}
+			rect(i * cell_size, j * cell_size, cell_size, cell_size);
+		}
+	}
 }
